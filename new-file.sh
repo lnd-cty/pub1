016c9a3f7c2a284eb0f09e2772a2f1e5d131bd39
LongND <ndlongit@gmail.com>
2024-07-31 15:35:34 +0700

    u

--- new-file.sh
+++ new-file.sh
@@ -1,24 +1,20 @@
-# v1.0.2
+## V1.0.0
 
 BASE_DIR=./temp
-# rm -fr $BASE_DIR
-# mkdir -p $BASE_DIR
-# chmod 777 $BASE_DIR
+rm -fr $BASE_DIR
+mkdir -p $BASE_DIR
+chmod 777 $BASE_DIR
 cd $BASE_DIR
 
 TIMESTAMP=$(date -d "now" +%Y-%m-%d__%H-%M-%S)
-#WORK_DIR="Folder__${TIMESTAMP}"
+WORK_DIR="Folder__${TIMESTAMP}"
+echo "Working folder is: ${WORK_DIR}"
 
-WORK_DIR=Work_Folder
+mkdir $WORK_DIR # && chmod 777 ./$WORK_DIR
 
-#echo "Working folder is: ${WORK_DIR}"
-
-# mkdir $WORK_DIR # && chmod 777 ./$WORK_DIR
-
-# git config --global http.sslVerify false
-# git clone https://github.com/lnd-cty/pub1.git $WORK_DIR
+git config --global http.sslVerify false
+git clone https://github.com/lnd-cty/pub1.git $WORK_DIR
 
 cd ./$WORK_DIR
 MAIN_FILE=main.sh
-chmod 111 $MAIN_FILE
-$MAIN_FILE
+chmod 111 ./$MAIN_FILE && ./$MAIN_FILE
